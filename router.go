b5d21d1a04ea5f10017c33e8f7788a4de48b162d
Hain-Lee Hsueh <hainleeh@goalbookapp.com>
2023-07-25 14:56:50 -0700

    use muxtrace.Router

--- router.go
+++ router.go
@@ -1,11 +1,12 @@
 package easyroute
 
 import (
-	"github.com/gorilla/mux"
-	gobrake "gopkg.in/airbrake/gobrake.v2"
 	"net/http"
 	"net/http/pprof"
 	"time"
+
+	muxtrace "gopkg.in/DataDog/dd-trace-go.v1/contrib/gorilla/mux"
+	gobrake "gopkg.in/airbrake/gobrake.v2"
 )
 
 type handlerFunc func(*Request)
@@ -20,7 +21,7 @@ type Logger struct {
 
 type Router struct {
 	// Inherit a mux router
-	*mux.Router
+	*muxtrace.Router
 
 	beforeHandler beforeHandlerFunc
 	logger        Logger
@@ -33,7 +34,7 @@ type Router struct {
 // NewRouter creates a new easyroute Router object with the provided
 // before handler and logger struct
 func NewRouter(beforeFn beforeHandlerFunc, logger Logger) Router {
-	muxRouter := mux.NewRouter()
+	muxRouter := muxtrace.NewRouter()
 
 	router := Router{
 		muxRouter,
@@ -45,6 +46,10 @@ func NewRouter(beforeFn beforeHandlerFunc, logger Logger) Router {
 	}
 
 	return router
+}
+
+func (g *Router) SetMuxTraceRouter(r *muxtrace.Router) {
+	g.Router = r
 }
 
 func (g *Router) ActivateProfiling() {
@@ -69,7 +74,7 @@ func (g *Router) EnableAirbrake(airbrakeId int64, airbrakeKey string) {
 // SubRoute creates a new easyroute Router off the base router with provided
 // prefix. This preserves the same before handler.
 func (g *Router) SubRoute(prefix string) Router {
-	muxRouter := g.PathPrefix(prefix).Subrouter()
+	muxRouter := muxtrace.WrapRouter(g.PathPrefix(prefix).Subrouter())
 
 	router := Router{
 		muxRouter,
@@ -88,7 +93,7 @@ func (g *Router) SubRoute(prefix string) Router {
 // The routes in this router will now run through first the parent(base) router's
 // before handler and then this router's before handler.
 func (g *Router) SubRouteC(prefix string, beforeFn beforeHandlerFunc) Router {
-	muxRouter := g.PathPrefix(prefix).Subrouter()
+	muxRouter := muxtrace.WrapRouter(g.PathPrefix(prefix).Subrouter())
 
 	router := Router{
 		muxRouter,
